209fe3f0e4b8bc542b46f46d4d705cbb878aa42e
Shujie Fan <gn8008@hotmail.com>
2018-11-13 14:35:08 -0700

    Add new script

--- zn_sites_rdf.py
+++ zn_sites_rdf.py
@@ -6,17 +6,19 @@ u = mda.Universe('step6.6.part0001.gro','step7_1000ns_dt_500_center.xtc')
 
 Zn1 = u.select_atoms('name ZND and resid 24905')
 Zn2 = u.select_atoms('name ZND and resid 24900')
+Zn3 = u.select_atoms('name ZND')
 
-Bsites1 = u.select_atoms('((name O* and resname ASP) or (name N* and resname HSD)) and around 5.0 (name ZND and resid 24905)')
-Bsites2 = u.select_atoms('((name O* and resname ASP) or (name N* and resname HSD)) and around 5.0 (name ZND and resid 24900)')
+Bsites1 = u.select_atoms('((name O* and resname ASP) or (name N* and resname HSD)) and around 4.0 (name ZND and resid 24905)')
+Bsites2 = u.select_atoms('((name O* and resname ASP) or (name N* and resname HSD)) and around 4.0 (name ZND and resid 24900)')
 
 ags = [[Zn1 ,Bsites1], [Zn2 ,Bsites2]]
 
-rdf = InterRDF_s(u, ags, nbins = 200, range=(0.0, 5.0), density=False)
+rdf = InterRDF_s(u, ags, nbins = 200, range=(1.0, 4.0), density=False)
 rdf.run()
 rdf.get_cdf()
 
-np.save('rdf.dat', rdf.rdf)
-np.save('indices.dat', rdf.indices)
-np.save('cdf.dat', rdf.cdf)
+np.savetxt('bins.dat', rdf.bins)
+np.save('rdf', rdf.rdf)
+np.save('indices', rdf.indices)
+np.save('cdf', rdf.cdf)
 
